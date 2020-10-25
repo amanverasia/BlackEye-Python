697270de1ac9a955517438078e76670c32d68909
Aman Verasia <verasiaaman3@gmail.com>
2020-10-25 23:27:12 +0530

    Create termux.sh

---
+++ termux.sh
@@ -0,0 +1,5 @@
+sudo apt install git php
+git clone https://github.com/shabbysinger/BlackEye-Python.git
+cd BlackEye-Python
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+unzip ngrok-stable-linux-arm.zip && rm -f ngrok-stable-linux-arm.zip

73ef7f58fc5c76c33a5936c35b0136f07fdb7493
Aman Verasia <verasiaaman3@gmail.com>
2020-10-26 14:10:06 +0530

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -2,4 +2,4 @@ apt install git php python openssh -y
 git clone https://github.com/shabbysinger/BlackEye-Python.git
 cd ~/.ssh && ssh-keygen
 cd
-cd BlackEye-Python
+cd BlackEye-Python && rm -f termux.sh install.sh

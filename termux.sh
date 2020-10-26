f4f0ad1c1a1c1a743c2b4fd0f26ba913574aeea7
Aman Verasia <verasiaaman3@gmail.com>
2020-10-26 12:51:41 +0530

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -2,10 +2,5 @@ apt install git php python openshh -y
 pkg install openssh -y
 git clone https://github.com/shabbysinger/BlackEye-Python.git
 cd ~/.ssh && ssh-keygen
-
-
 cd
-
 cd BlackEye-Python
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
-unzip ngrok-stable-linux-arm.zip && rm -f ngrok-stable-linux-arm.zip

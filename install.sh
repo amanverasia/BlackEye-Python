0148e66bd9bb0093cc75bc92d3ea7424373aa3bc
Aman Verasia <35035869+amanverasia@users.noreply.github.com>
2022-04-05 12:04:36 +0530

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-sudo apt install git php
-git clone https://github.com/shabbysinger/BlackEye-Python.git
-cd BlackEye-Python
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip ngrok-stable-linux-amd64.zip && rm -f ngrok-stable-linux-amd64.zip

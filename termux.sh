1682abe6608a66ada89911f988d10e8c6b29a4e3
Aman Verasia <35035869+amanverasia@users.noreply.github.com>
2022-04-05 12:04:45 +0530

    Delete termux.sh

--- termux.sh
+++
@@ -1,5 +0,0 @@
-apt install git php python openssh -y
-git clone https://github.com/shabbysinger/BlackEye-Python.git
-cd ~/.ssh && ssh-keygen
-cd
-cd BlackEye-Python && rm -f termux.sh install.sh main.py LICENSE README.md
